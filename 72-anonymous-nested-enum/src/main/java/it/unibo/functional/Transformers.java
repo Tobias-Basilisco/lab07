5f17be377250643dc30ab869269aea2a41d9b415
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-11-08 17:27:21 +0100

    72 2) flatten compila

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -75,7 +75,7 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
